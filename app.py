d651bcfea92ee39f712e9e9d1bd817929a18e47e
angel0314ist <alc.quishpe@yavirac.edu.ec>
2026-06-08 11:08:59 -0500

    Version 2.0.0

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+VERSION = "2.0.0"
+
+@app.route("/")
+def inicio():
+
+    try:
+
+        conexion = psycopg2.connect(
+            host="db",
+            database="empresa",
+            user="admin",
+            password="admin123"
+        )
+
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT nombre FROM clientes")
+
+        clientes = cursor.fetchall()
+
+        lista = ""
+
+        for cliente in clientes:
+            lista += f"<li>{cliente[0]}</li>"
+
+        cursor.close()
+        conexion.close()
+
+        return f"""
+        <h1>Aplicación Flask</h1>
+        <h2>Versión {VERSION}</h2>
+
+        <p>Conexión exitosa a PostgreSQL</p>
+
+        <h3>Clientes Registrados</h3>
+
+        <ul>
+            {lista}
+        </ul>
+        """
+
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
